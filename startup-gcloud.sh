14424ab9af7d43df5a542793365ed1b0c1dabeb2
Davod <davidkingnt@gmail.com>
2019-06-05 19:14:34 -0400

    Create startup-gcloud.sh

---
+++ startup-gcloud.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Local Environment variables
+PUID=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['uid'])")
+PGID=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['gid'])")
+LOCAL_HOME=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['homeDirectory'])")
+
+# Create the directories
+mkdir -p "LOCAL_HOME"
+chown -R "$PUID:$PGID" "$LOCAL_HOME"
