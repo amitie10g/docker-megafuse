d2b361225dce43f9466a44a2ae5fab4c438f2aea
Davod <davidkingnt@gmail.com>
2019-06-06 05:22:48 -0400

    Update startup-gcloud.sh

--- startup-gcloud.sh
+++ startup-gcloud.sh
@@ -5,7 +5,7 @@ PUID=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/use
 PGID=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['gid'])")
 LOCAL_HOME=$(curl -fSs "http://metadata.google.internal/computeMetadata/v1/oslogin/users?pagesize=1" -H "Metadata-Flavor: Google" | python -c "import sys, json; print(json.load(sys.stdin)['loginProfiles'][0]['posixAccounts'][0]['homeDirectory'])")
 
-CONFIG_PATH="$LOCAL_HOME/config"
+CACHE_PATH="$LOCAL_HOME/cache"
 
 # Create the directories
 mkdir -p "$CONFIG_PATH"
