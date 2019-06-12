7e838ea727ea4e6e7778f6d5883c15c656f2a97a
Davod <davidkingnt@gmail.com>
2019-06-11 23:14:57 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,22 @@
+PUID=$(id -u)
+PGID=$(id -g)
+
+USERNAME=<MEGA username>
+PASSWORD=<MEGA password>
+APIKEY=<Mega API key>
+
+CONF_PATH=$HOME/config
+CACHE_PATH=$HOME/cache
+
+docker run -t -i -d \
+--name=megafuse \
+-e PUID=$PUID \
+-e PGID=$PGID \
+-e USERNAME=$USERNAME \
+-e PASSWORD=$PASSWORD \
+-e APIKEY=$APIKEY \
+-v $CONF_PATH:/config \
+--device=/dev/fuse \
+--restart no \
+--privileged \
+amitie10g/megafuse:latest
